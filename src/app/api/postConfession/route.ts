df69a44097e0ecbce8535b45c3dedbf114aa90bc
laughing-nerd <riturajpaul99@gmail.com>
2023-10-28 12:18:38 +0530

    Fix:Ratelimit

--- src/app/api/postConfession/route.ts
+++ src/app/api/postConfession/route.ts
@@ -1,12 +1,12 @@
 import { Ratelimit } from "@upstash/ratelimit";
 import { Redis } from "@upstash/redis";
 import { NextRequest, NextResponse } from "next/server";
-import { ID } from "appwrite"; 
+import { ID } from "appwrite";
 import { databases } from "@/appwrite/config";
 
 const redis = new Redis({
-  url: 'UPSTASH_REDIS_URL',
-  token: 'UPSTASH_REDIS_TOKEN',
+  url: process.env.UPSTASH_REDIS_URL!,
+  token: process.env.UPSTASH_REDIS_TOKEN!,
 })
 const ratelimit = new Ratelimit({
   redis: redis,
@@ -16,24 +16,37 @@ const ratelimit = new Ratelimit({
 export async function POST(request: NextRequest) {
   const ip = request.headers.get("X-forwarded-for") ?? ""
   const result = await ratelimit.limit(ip)
-  if (!result.success){
-    return NextResponse.json({ success: false, message: "Too many requests! Please try again later" })
+
+  console.log(result.limit)
+  console.log(result.remaining)
+  if (!result.success) {
+    return new NextResponse(JSON.stringify({ success: false, message: "Too many requests! Please try again later" }), {
+      status: 429,
+    })
   }
-  
-  
+
+
   const confession = await request.json();
-  if (confession.length != 0){
-    try{
+  if (confession.length != 0) {
+    try {
       await databases.createDocument(process.env.NEXT_PUBLIC_DATABASE_ID!, process.env.NEXT_PUBLIC_COLLECTION_ID!, ID.unique(), {
         confession: confession,
         comments: []
       })
     }
-    catch(err){
+    catch (err) {
       console.log(err)
-      return NextResponse.json({ success: false, message: "Something went wrong! Your confession couldn't be added" });
+      return NextResponse.json(JSON.stringify({ success: false, message: "Something went wrong! Your confession couldn't be added" }), {
+        status: 500
+      });
     }
-    return NextResponse.json({ success: true });
+    return new NextResponse(JSON.stringify({ success: true }), {
+      status: 200,
+      headers: {
+        "X-RateLimit-Limit": String(result.limit),
+        "X-RateLimit-Remaining": String(result.remaining)
+      }
+    });
   }
   return NextResponse.json({ success: false, message: "You are not allowed to send empty confessions. Don't be a wuss and confess something!" });
 }
